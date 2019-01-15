94fde5a826088df122bf2615468ec5f12c11b3ba
github username <github email address>
2019-01-14 23:23:52 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
 class Instructor
 
+  attr_accessor :instructor
+
+  @@all = []
+
+  def initialize(instructor)
+    @instructor = instructor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def test_of_student(student,test_name)
+    BoatingTest.all.find {|test| test.student.name == student && test.test_name == test_name}
+  end
+
+  def pass_student(student,test_name)
+    if test_of_student(student,test_name).class == BoatingTest
+      test_of_student(student,test_name).test_status = 'passed'
+    else
+      BoatingTest.new(Student.new(student),test_name,"passed",self)
+    end
+  end
+
+  def fail_student(student,test_name)
+    if test_of_student(student,test_name).class == BoatingTest
+      test_of_student(student,test_name).test_status = 'failed'
+    else
+      BoatingTest.new(Student.new(student),test_name,"failed",self)
+    end
+  end
 end
