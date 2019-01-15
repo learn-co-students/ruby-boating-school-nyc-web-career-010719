6b6291d90e579f0ef413bafbe713d862ebb2edb4
Linya Hu <lh62594@yahoo.com>
2019-01-15 15:33:58 -0500

    first upload

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,55 @@
 class Instructor
+  attr_reader :name
 
-end
+  @@all = []
+
+  ############## CLASS METHODS ##############
+  def self.all
+    @@all
+  end
+
+  ############## INSTANCE METHODS ##############
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def boating_tests
+    BoatingTest.all.select{|test| test.instructor == self}
+  end
+
+  def students
+    self.boating_tests.map{|test| test.student}
+  end
+
+  def pass_student(student_name, test_name)
+    boating_test = self.boating_tests.find{|test| test.test_name == test_name && test.student.first_name == student_name}
+
+    if boating_test == nil
+      student = self.students.find{|student| student.first_name == student_name}
+      
+      boating_test = BoatingTest.new(student, test_name, "passed", self)
+    else
+      boating_test.test_status = "passed"
+    end
+    boating_test
+  end
+
+
+  def fail_student(student_name, test_name)
+    boating_test = self.boating_tests.find{|test| test.test_name == test_name && test.student.first_name == student_name}
+
+    if boating_test == nil
+      student = self.students.find{|student| student.first_name == student_name}
+
+      boating_test = BoatingTest.new(student, test_name, "failed", self)
+    else
+      boating_test.test_status = "failed"
+    end
+    boating_test
+
+  end
+
+
+end #end of Instructor class
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,41 @@
 class Student
+  attr_reader :first_name
 
-end
+  @@all = []
+
+  ############## CLASS METHODS ##############
+  def self.all
+    @@all
+  end
+
+  def self.find_student(first_name)
+    self.all.find{|student| student.first_name == first_name}
+  end
+
+  ############## INSTANCE METHODS ##############
+  def initialize(first_name)
+    @first_name = first_name
+
+    @@all << self
+  end
+
+  def boating_tests
+    BoatingTest.all.select{|test| test.student == self}
+  end
+
+  def instructors
+    self.boating_tests.map{|test| test.instructor}
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    new_test = BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def grade_percentage
+    total_tests = self.boating_tests.count
+    passed_tests = self.boating_tests.select{|test| test.test_status == "passed"}.count
+    passed_tests / total_tests
+  end
+
+
+end #end of Student class
