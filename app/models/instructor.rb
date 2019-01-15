83e5b63d79a4399a5d0e56a7256b4a208eddeb24
davidfloyd91 <davidfloyd91@gmail.com>
2019-01-15 00:38:38 -0500

    Done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
 class Instructor
 
+  attr_reader :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def pass_or_fail(student, test_name, p_or_f)
+    test = BoatingTest.all.find do | t |
+      t.student.name == student && t.test_name == test_name && t.status != p_or_f
+    end
+    if !test
+      test = BoatingTest.new(student, test_name, p_or_f, self)
+    else
+      test.status = p_or_f
+    end
+    test
+  end
+
+  def pass_student(student, test_name)
+    pass_or_fail(student, test_name, "passed")
+  end
+
+  def fail_student(student, test_name)
+    pass_or_fail(student, test_name, "failed")
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,10 +4,37 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 202", "passed", krabs)
+
+no_crashing
+power_steering_failure
+power_steering_pass
+
+BoatingTest.all
+
+power_steering_pass.student
+
+Student.find_student("Spongebob")
+
+patrick.boating_tests
+
+# puff.pass_student("Spongebob", "Don't Crash 101")
+
+sandy = Student.new("Sandy")
+# puff.pass_student("Sandy", "cool test")
+#
+# my_leg = Student.new("My Leg Guy")
+# krabs.fail_student("My Leg Guy", "way cool test")
+
+# puff.pass_student("Patrick", "Power Steering 202")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
