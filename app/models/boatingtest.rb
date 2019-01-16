2c9431afab59077c4d895a2cf939dcf817aba586
github username <github email address>
2019-01-15 20:17:36 -0500

    finished

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,22 @@
 class BoatingTest
+  attr_reader :student, :instructor
+  attr_accessor :test_name, :test_status
 
-end
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(test_name, test_status, instructor, student)
+    @test_name = test_name
+    @test_status = test_status
+    @instructor = instructor
+    @student = student
+    @@all << self
+  end
+
+
+
+
+end #end of class
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Instructor
 
+  attr_reader :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+#this will access all my (instructor's) boating tests
+  def boating_test
+    BoatingTest.select do |test|
+    test.instructor == self
+  end
 end
+
+#find my (instructor's) students
+  def student
+    boating_test.map do |test|
+      test.student
+  end
+end
+
+def self.pass_student(stu, tee)
+  boattest = BoatingTest.all.find do |bt|
+    bt.test_name == tee
+    bt.student == stu
+  end
+  boattest.test_status = "Passed"
+end
+
+def self.fail_student(stu, tee)
+  boattest = BoatingTest.all.find do |bt|
+    bt.test_name == tee
+    bt.student == stu
+  end
+  boattest.test_status = "failed"
+end
+
+
+end # end of class
